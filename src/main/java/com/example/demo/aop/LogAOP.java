dff242e38a682f430e82bbeb154262032e8812bd
Sargsyan <artem2001sarg@gmail.com>
2021-07-22 16:16:24 +0300

    aop

---
+++ src/main/java/com/example/demo/aop/LogAOP.java
@@ -0,0 +1,20 @@
+package com.example.demo.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Slf4j
+@Component
+public class LogAOP {
+    @Pointcut("execution(* *(..)) && within(com.example..*)")
+    private void logPointcut(){}
+
+    @Before("logPointcut()")
+    public void doLogBefore(){
+        log.info("Succs");
+    }
+}
